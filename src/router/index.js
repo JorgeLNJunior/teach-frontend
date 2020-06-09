8bd266697d962b5c66677550c3c8931d2d0df73d
JorgeLNJunior <jorgelnjunior@gmail.com>
2020-06-09 15:17:10 -0300

    Limitar a rota principal somente a usuários logados

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+import UserService from '../services/UserService'
+import decode from 'jwt-decode'
+
 Vue.use(VueRouter)
 
 const routes = [
@@ -27,7 +30,8 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: () => import('../modules/home/views/Home')
+    component: () => import('../modules/home/views/Home'),
+    meta: { requiresAuth: true }
   }
 ]
 
@@ -35,6 +39,26 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach(async (to, from, next) => {
+  if (to.matched.some(route => route.meta.requiresAuth)) {
+    const token = localStorage.getItem('token')
+
+    if (!token) {
+      return next('/login')
+    }
+
+    try {
+      const decodedToken = decode(token)
+      await UserService.getByID(decodedToken.uid)
+      return next()
+    } catch (error) {
+      console.log(error)
+      return next('/login')
+    }
+  }
+  next()
 })
 
 export default router
